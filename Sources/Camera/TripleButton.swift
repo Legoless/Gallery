b6ac1f92dacfc705065d06dcf19c76e47ed42774
Dal Rupnik <legoless@gmail.com>
2018-09-08 11:55:52 +0200

    Update TripleButton.swift

--- Sources/Camera/TripleButton.swift
+++ Sources/Camera/TripleButton.swift
@@ -2,17 +2,17 @@ import UIKit
 
 class TripleButton: UIButton {
 
-  struct State {
+  struct TripleButtonState {
     let title: String
     let image: UIImage
   }
 
-  let states: [State]
+  let states: [TripleButtonState]
   var selectedIndex: Int = 0
 
   // MARK: - Initialization
 
-  init(states: [State]) {
+  init(states: [TripleButtonState]) {
     self.states = states
     super.init(frame: .zero)
     setup()
